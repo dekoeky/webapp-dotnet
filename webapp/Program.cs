8143595ff269f3d97845caa5d4742941af1851a4
Wesley Van Koeckhoven <wvk@outlook.be>
2023-08-20 16:20:53 +0200

    use appsettings for httpsredirect

--- webapp/Program.cs
+++ webapp/Program.cs
@@ -13,7 +13,8 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+if (app.Configuration.GetValue("UseHttpsRedirection", true))
+    app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
